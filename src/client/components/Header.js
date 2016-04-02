407c081f91a57163a911260bb6b1eceb46e731ad
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-04-02 15:37:47 -0400

    refactor

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    const styles = {
-      main: {
-        textAlign: 'center',
-        fontFamily: 'Source Sans Pro',
-        fontSize: 50,
-      },
-    };
     return <h1 style={styles.main}>PowerChord Recipes</h1>;
   }
 }
+
+const styles = {
+  main: {
+    textAlign: 'center',
+    fontFamily: 'Source Sans Pro',
+    fontSize: 50,
+  },
+};
 
 export default Header;
